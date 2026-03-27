ed3dac9230293dfbee3a809d0314cb17c08c385f
lakshay Kamboj <lkamboj2300@gmail.com>
2026-03-27 18:38:17 +0530

    created basic go server

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Lakshay309/student-api-go/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// setup router
+	router:=http.NewServeMux()
+
+	router.HandleFunc("GET /",func(w http.ResponseWriter,r *http.Request){
+		w.Write([]byte("Welcome to student api"))
+	})
+	// setup server
+	server:=http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("server started %s",cfg.HTTPServer.Addr)
+	err:=server.ListenAndServe()
+	if err!=nil{
+		log.Fatal("failed to start server");
+	}
 }
